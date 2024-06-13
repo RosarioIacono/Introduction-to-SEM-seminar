67c8c70b04e8c56778d0a6e22c5e300b26595302
Rosario Iacono <45822340+RosarioIacono@users.noreply.github.com>
2024-06-13 14:48:51 +0200

    Update SEM-example-BOOEKseminar.R

--- SEM-example-BOOEKseminar.R
+++ SEM-example-BOOEKseminar.R
@@ -1,2 +1,10 @@
 # Example of covariance based Structural Equation Modeling testing
+# Source: https://cran.r-project.org/web/packages/lavaanPlot/vignettes/Intro_to_lavaanPlot.html
 library(lavaan)
+library(lavaanPlot)
+
+model <- 'mpg ~ cyl + disp + hp
+          qsec ~ disp + hp + wt'
+
+fit <- sem(model, data = mtcars)
+summary(fit)
